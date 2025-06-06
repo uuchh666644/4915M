8886805dd1f95563a7826e9bde0bf383e6eaeb24
uuchh666644 <davidchunhowang@gmail.com>
2025-06-06 19:54:19 +0800

    Add files via upload

---
+++ DeliverySystem/DeliverySystem/ReceiptClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Dispatch_Processing_System
+{
+    public class ReceiptClass
+    {
+        DBconnect connect = new DBconnect();
+
+        public DataTable GetReceiptList()
+        {
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `receipts`", connect.getconnection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+    }
+}
