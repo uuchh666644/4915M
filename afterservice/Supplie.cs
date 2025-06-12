075cc656cccb3941d5e154a36f5ff0a8dfba85f8
uuchh666644 <davidchunhowang@gmail.com>
2025-06-12 18:50:04 +0800

    Add files via upload

---
+++ afterservice/Supplie.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace afterservice
+{
+    public partial class Supplie : Form
+    {
+        GuestClass record = new GuestClass();
+        public Supplie()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ADD_Click(object sender, EventArgs e)
+        {
+            string supplier = textBox_supplier.Text;
+            string iteamname = textBox_iteamname.Text;
+            string quantity = textBox_quantity.Text;
+            string price = textBox_price.Text;
+            DateTime indate = dateTimePicker1.Value;
+
+            if (verify())
+            {
+                try
+                {
+                    if (record.insertinrecord(supplier, iteamname, quantity, price, indate))
+                    {
+                        MessageBox.Show("Now the Purchase order added", "Add Purchase order", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Emty  Field", "Add record", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            bool verify()
+            {
+                if ((textBox_supplier.Text == "") || (textBox_iteamname.Text == "") || (textBox_quantity.Text == "") || (textBox_price.Text == ""))
+                {
+                    return false;
+                }
+                else
+                    return true;
+            }
+
+
+
+        }
+
+        private void Supplie_Load(object sender, EventArgs e)
+        {
+            {
+                showTable();
+            }
+        }
+        public void showTable()
+        {
+            dataGridView1.DataSource = record.getRecordlist();
+
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            textBox_supplier.Clear();
+            textBox_iteamname.Clear();
+            textBox_quantity.Clear();
+            textBox_price.Clear();
+        }
+
+        private void textBox_supplier_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ afterservice/loginForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace afterservice
+{
+    public partial class loginForm : Form
+    {
+        GuestClass guest = new GuestClass();
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label5_MouseEnter(object sender, EventArgs e)
+        {
+            label5.ForeColor = Color.Red;
+        }
+
+        private void label5_MouseLeave(object sender, EventArgs e)
+        {
+            label5.ForeColor = Color.Transparent;
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            if(textBox_username.Text == "" || textBox_password.Text == "")
+            { 
+                MessageBox.Show("Need login data","Wrong Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                }
+            else 
+            {
+                
+                
+            string uname = textBox_username.Text;
+            string pass = textBox_password.Text;
+            DataTable table = guest.getList(new MySqlCommand("SELECT * FROM `user` WHERE `username` = '" + uname + "' AND `password` = '"+ pass +"'"));
+            if(table.Rows.Count > 0)
+            { 
+                menu mu = new menu();
+                this.Hide();
+                mu.Show();
+                
+                }
+            else
+            { 
+                MessageBox.Show("Your username and password are not exists","Wrong Login",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
