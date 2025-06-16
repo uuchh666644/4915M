c54106a0cb57a4845899c7aec27f19b675799b21
uuchh666644 <davidchunhowang@gmail.com>
2025-06-16 12:39:44 +0800

    Add files via upload

---
+++ DeliveryRecordForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace afterservice
+{
+    partial class DeliveryRecordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelDeliveryRecords = new System.Windows.Forms.Label();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelProduct = new System.Windows.Forms.Label();
+            this.labelGuestName = new System.Windows.Forms.Label();
+            this.labelOrderID = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxProduct = new System.Windows.Forms.TextBox();
+            this.textBoxGuestName = new System.Windows.Forms.TextBox();
+            this.textBoxOrderID = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.labelDeliveryRecords);
+            this.panel1.Location = new System.Drawing.Point(-5, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(809, 90);
+            this.panel1.TabIndex = 37;
+            // 
+            // labelDeliveryRecords
+            // 
+            this.labelDeliveryRecords.AutoSize = true;
+            this.labelDeliveryRecords.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelDeliveryRecords.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelDeliveryRecords.Location = new System.Drawing.Point(308, 38);
+            this.labelDeliveryRecords.Name = "labelDeliveryRecords";
+            this.labelDeliveryRecords.Size = new System.Drawing.Size(166, 21);
+            this.labelDeliveryRecords.TabIndex = 0;
+            this.labelDeliveryRecords.Text = "Delivery Records";
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.BackColor = System.Drawing.Color.Lime;
+            this.buttonPrint.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonPrint.Location = new System.Drawing.Point(426, 333);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrint.TabIndex = 36;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = false;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.Yellow;
+            this.buttonBack.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonBack.Location = new System.Drawing.Point(62, 398);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 35;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(286, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(340, 150);
+            this.dataGridView1.TabIndex = 34;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.Red;
+            this.buttonDelete.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDelete.Location = new System.Drawing.Point(307, 333);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 33;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.HotPink;
+            this.buttonUpdate.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonUpdate.Location = new System.Drawing.Point(184, 333);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 32;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonAdd.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonAdd.Location = new System.Drawing.Point(62, 333);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 31;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelEmail.Location = new System.Drawing.Point(62, 265);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(40, 13);
+            this.labelEmail.TabIndex = 30;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAddress.Location = new System.Drawing.Point(62, 237);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(54, 13);
+            this.labelAddress.TabIndex = 29;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelPhone.Location = new System.Drawing.Point(62, 209);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(42, 13);
+            this.labelPhone.TabIndex = 28;
+            this.labelPhone.Text = "Phone";
+            // 
+            // labelProduct
+            // 
+            this.labelProduct.AutoSize = true;
+            this.labelProduct.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelProduct.Location = new System.Drawing.Point(60, 181);
+            this.labelProduct.Name = "labelProduct";
+            this.labelProduct.Size = new System.Drawing.Size(51, 13);
+            this.labelProduct.TabIndex = 27;
+            this.labelProduct.Text = "Product";
+            // 
+            // labelGuestName
+            // 
+            this.labelGuestName.AutoSize = true;
+            this.labelGuestName.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelGuestName.Location = new System.Drawing.Point(62, 152);
+            this.labelGuestName.Name = "labelGuestName";
+            this.labelGuestName.Size = new System.Drawing.Size(73, 13);
+            this.labelGuestName.TabIndex = 26;
+            this.labelGuestName.Text = "GuestName";
+            // 
+            // labelOrderID
+            // 
+            this.labelOrderID.AutoSize = true;
+            this.labelOrderID.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelOrderID.Location = new System.Drawing.Point(60, 124);
+            this.labelOrderID.Name = "labelOrderID";
+            this.labelOrderID.Size = new System.Drawing.Size(55, 13);
+            this.labelOrderID.TabIndex = 25;
+            this.labelOrderID.Text = "OrderID";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(138, 255);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 22);
+            this.textBoxEmail.TabIndex = 24;
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.textBoxEmail_TextChanged);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(138, 227);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAddress.TabIndex = 23;
+            this.textBoxAddress.TextChanged += new System.EventHandler(this.textBoxAddress_TextChanged);
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(138, 199);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPhone.TabIndex = 22;
+            this.textBoxPhone.TextChanged += new System.EventHandler(this.textBoxPhone_TextChanged);
+            // 
+            // textBoxProduct
+            // 
+            this.textBoxProduct.Location = new System.Drawing.Point(138, 171);
+            this.textBoxProduct.Name = "textBoxProduct";
+            this.textBoxProduct.Size = new System.Drawing.Size(100, 22);
+            this.textBoxProduct.TabIndex = 21;
+            this.textBoxProduct.TextChanged += new System.EventHandler(this.textBoxProduct_TextChanged);
+            // 
+            // textBoxGuestName
+            // 
+            this.textBoxGuestName.Location = new System.Drawing.Point(138, 143);
+            this.textBoxGuestName.Name = "textBoxGuestName";
+            this.textBoxGuestName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxGuestName.TabIndex = 20;
+            this.textBoxGuestName.TextChanged += new System.EventHandler(this.textBoxGuestName_TextChanged);
+            // 
+            // textBoxOrderID
+            // 
+            this.textBoxOrderID.Location = new System.Drawing.Point(138, 115);
+            this.textBoxOrderID.Name = "textBoxOrderID";
+            this.textBoxOrderID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxOrderID.TabIndex = 19;
+            this.textBoxOrderID.TextChanged += new System.EventHandler(this.textBoxOrderID_TextChanged);
+            // 
+            // DeliveryRecordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelProduct);
+            this.Controls.Add(this.labelGuestName);
+            this.Controls.Add(this.labelOrderID);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxProduct);
+            this.Controls.Add(this.textBoxGuestName);
+            this.Controls.Add(this.textBoxOrderID);
+            this.Name = "DeliveryRecordForm";
+            this.Text = "DeliveryRecordForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelDeliveryRecords;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelProduct;
+        private System.Windows.Forms.Label labelGuestName;
+        private System.Windows.Forms.Label labelOrderID;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxProduct;
+        private System.Windows.Forms.TextBox textBoxGuestName;
+        private System.Windows.Forms.TextBox textBoxOrderID;
+    }
+}
