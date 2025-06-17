bb71b7b6c1e15bfabf82e0969535326b245490b1
uuchh666644 <davidchunhowang@gmail.com>
2025-06-17 13:59:58 +0800

    Add files via upload

---
+++ DeliveryOrderData.cs
@@ -0,0 +1,9 @@
+﻿public class DeliveryOrderData
+{
+    public string Product { get; set; }
+    public int Quantity { get; set; }
+    public string CustomerName { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+}
